88c4fbc4463cc95701db443157db22d5a401e3b6
takuyaone <one.amaza@gmail.com>
2021-10-02 11:37:45 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -69,7 +69,7 @@ class OwnersController extends Controller
         ]);
         return redirect()
         ->route('admin.owners.index')
-        ->with('message','オーナー登録を実施しました。');
+        ->with(['message'=>'オーナー登録を実施しました。','status'=>'info']);
     }
 
     /**
@@ -110,10 +110,10 @@ class OwnersController extends Controller
         $owner->email=$request->email;
         $owner->password=Hash::make($request->password);
         $owner->save();
-        
+
         return redirect()
         ->route('admin.owners.index')
-        ->with('message','オーナー情報を更新しました。');
+        ->with(['message'=>'オーナー情報を更新しました。','status'=>'info']);
     }
 
     /**
@@ -124,6 +124,20 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete(); //ソフトデリート
+        return redirect()
+            ->route('admin.owners.index')
+            ->with(['message'=>'オーナー情報を削除しました。','status'=>'alert']);
+
+    }
+
+    public function expiredOwnerIndex(){
+        $expiredOwners=Owner::onlyTrashed()->get();
+        return view('admin.expired-owners',compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id){
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index');
     }
 }
--- resources/views/admin/owners/create.blade.php
+++ resources/views/admin/owners/create.blade.php
@@ -41,7 +41,7 @@
                         </div>
                         <div class="p-2 w-full flex justify-around mt-4">
                             <button type="button" onclick="location.href='{{ route('admin.owners.index') }}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                            <button type="submit" class=" text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">登録する</button>
+                            <button type="submit" class=" text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">登録する</button>
 
                         </div>
                     </div>
