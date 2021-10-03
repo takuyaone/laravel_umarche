2167437a84cb083b56c2aacc79cb3de55f7f51a9
takuyaone <one.amaza@gmail.com>
2021-10-04 07:51:59 +0900

    image delete

--- resources/views/owner/images/edit.blade.php
+++ resources/views/owner/images/edit.blade.php
@@ -16,8 +16,8 @@
                         <div class="-m-2">
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
-                                    <label for="title" class="leading-7 text-sm text-gray-600">画像のタイトル</label>
-                                    <input type="text" id="title" name="title" value="{{$image->title}}" accept="image/png,image/jpeg,image/jpg" 　class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <label for="title" class="leading-7 text-sm text-gray-600">画像タイトル</label>
+                                    <input type="text" id="title" name="title" value="{{$image->title}}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
                             </div>
                             <div class="p-2 w-1/2 mx-auto">
@@ -31,11 +31,25 @@
                                 <button type="submit" class=" text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">更新する</button>
                             </div>
                         </div>
-
-
+                    </form>
+                    <form id="delete_{{$image->id}}" method="post" action="{{route('owner.images.destroy',['image'=> $image->id ])}}">
+                        @csrf
+                        @method('delete')
+                        <div class="p-2 w-full flex justify-around mt-28">
+                            <a href="#" data-id="{{$image->id}}" onclick="deletePost(this)" class=" text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded">削除する</a>
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してよろしいですか？')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
+    </script>
+
 </x-app-layout>
