5767298b17491ba371239f650815461df2c0832e
takuyaone <one.amaza@gmail.com>
2021-10-04 06:45:48 +0900

    image,create,validation

--- resources/views/owner/images/index.blade.php
+++ resources/views/owner/images/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-flash-message status="session('status')" />
                     <div class="flex justify-end mb-4">
-                        <button onclick="location.href='{{ route('owner.images.index')}}'" class=" text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">新規登録する</button>
+                        <button onclick="location.href='{{ route('owner.images.create')}}'" class=" text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">新規登録する</button>
                     </div>
 
                     @foreach($images as $image)
