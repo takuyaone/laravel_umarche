b8e451f2e0f78a75353abd784febaeda2d8ee05e
takuyaone <one.amaza@gmail.com>
2021-10-05 08:26:00 +0900

    category

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
 use App\Models\Product;
-use App\Models\SecondaryCategory;
+use App\Models\PrimaryCategory;
 use App\Models\Owner;
+use App\Models\Shop;
 
 
 
@@ -54,7 +55,11 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $shops=Shop::where('owner_id',Auth::id())->select('id','name')->get();
+        $images=Image::where('owner_id',Auth::id())->select('id','title','filename')->orderBy('updated_at','desc')->get();
+        $categories=PrimaryCategory::with('secondary')->get();
+
+        return view('owner.products.create',compact('shops','images','categories'));
     }
 
     /**
