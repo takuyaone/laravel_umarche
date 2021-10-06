aca5a4e4b999572e8166561d6d8ba42660689409
takuyaone <one.amaza@gmail.com>
2021-10-07 00:15:38 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+        public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+    
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -46,5 +51,12 @@ class ItemController extends Controller
         // dd($products);
         // $products=Product::all();
         return view('user.index',compact('products'));
+    }
+
+    public function show($id)
+    {
+        $product=Product::findOrFail($id);
+
+        return view('user.show',compact('product'));
     }
 }
