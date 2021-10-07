d4b37f7bbe8bde3dc16b462d3a4d5cbe846da120
takuyaone <one.amaza@gmail.com>
2021-10-07 10:29:55 +0900

    cart relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use App\Models\Shop;
 use App\Models\Image;
 use App\Models\SecondaryCategory;
 use App\Models\Stock;
-
+use App\Models\User;
 
 
 
@@ -66,6 +66,11 @@ class Product extends Model
         return $this->hasMany(Stock::class);
     }
 
+    public function users()
+    {
+        return $this->belongsToMany(Product::class, 'carts')
+            ->withPivot(['id', 'quantity']);
+    }
 
 
 }
